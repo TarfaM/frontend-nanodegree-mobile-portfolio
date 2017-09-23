08cb8b5298dfb0509fde758723e8befdff55c546
tarfa <tarfa.mansour@gmail.com>
2017-09-24 01:02:04 +0300

    Submit
    Submit

--- views/js/main.js
+++ views/js/main.js
@@ -527,15 +527,16 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
+  var elem = document.createElement('img');
+  for (var i = 0; i < 26; i++) {
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = 100/s+'px';
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
-  }
+    elem=elem;
+    }
+  document.querySelector("#movingPizzas1").appendChild(elem);
   updatePositions();
 });
